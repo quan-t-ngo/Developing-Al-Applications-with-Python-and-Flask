0bae044618f296dbb1a4aad3090c4ff9451753ea
Quan T. Ngo <90666138+quan-t-ngo@users.noreply.github.com>
2026-07-02 22:08:43 +0700

    Implement unit tests for emotion_detector
    Added unit tests for the emotion_detector function to verify correct emotion classification.

---
+++ test_emotion_detection.py
@@ -0,0 +1,29 @@
+import unittest
+
+from EmotionDetection import emotion_detector
+
+
+class TestEmotionDetector(unittest.TestCase):
+    """
+    Unit tests for the emotion_detector function.
+    """
+
+    def test_emotion_detector(self):
+        response = emotion_detector("I am glad this happened")
+        self.assertEqual(response["dominant_emotion"], "joy")
+
+        response = emotion_detector("I am really mad about this")
+        self.assertEqual(response["dominant_emotion"], "anger")
+
+        response = emotion_detector("I feel disgusted just hearing about this")
+        self.assertEqual(response["dominant_emotion"], "disgust")
+
+        response = emotion_detector("I am so sad about this")
+        self.assertEqual(response["dominant_emotion"], "sadness")
+
+        response = emotion_detector("I am really afraid that this will happen")
+        self.assertEqual(response["dominant_emotion"], "fear")
+
+
+if __name__ == "__main__":
+    unittest.main()
